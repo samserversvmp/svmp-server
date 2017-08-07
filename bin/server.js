752823430a573a3a29affc97c695f126810a7801
Danila Balabaev <shattenjagger@yandex.ru>
2017-08-07 13:36:09 +0300

    r

--- bin/server.js
+++ bin/server.js
@@ -1,37 +1,17 @@
-#!/usr/bin/env node
-
-/*
- * Copyright 2013-2014 The MITRE Corporation, All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this work except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author Dave Bryson
- *
- */
 var
-    svmp = require(__dirname + '/../lib/svmp'),
+    sam = require(__dirname + '/../lib/sam'),
     proxy = require(__dirname + '/../lib/server/proxy'),
     webSocket = require(__dirname + '/../lib/server/websocket');
 
-svmp.init();
+sam.init();
 
-var port = svmp.config.get('port');
-var with_tls = svmp.config.get('enable_ssl');
+var port = sam.config.get('port');
+var with_tls = sam.config.get('enable_ssl');
 
 webSocket.createServer(undefined,proxy.handleConnection).listen(port);
-svmp.logger.info('Listening on port %d', port);
+sam.logger.info('Listening on port %d', port);
 if (with_tls) {
-    svmp.logger.info('SSL is enabled');
+    sam.logger.info('SSL is enabled');
 } else {
-    svmp.logger.info('SSL is disabled');
+    sam.logger.info('SSL is disabled');
 }
